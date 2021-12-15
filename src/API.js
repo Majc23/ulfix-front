d68b8473b552b6114ff67e479a6392aaef474798
Majc23 <76822349+Majc23@users.noreply.github.com>
2021-12-15 16:44:06 -0600

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,38 +1,6 @@
 const api = "http://localhost:3500";
 
 
-// Generate a unique token for storing your bookshelf data on the backend server.
-let token = localStorage.token
-if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
-
-if( localStorage.getItem("acessToken") ){
-    token = JSON.parse(localStorage.getItem("acessToken"))
-    console.log(token);
-}
-
-
-
-/*
-export const createUser = async (name, email, password) =>{
-  let response = await fetch(`${api}/api/users`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: name,
-      email: email,
-      password: password
-    })
-  });
-  let json = await response.json();
-  console.log(json);
-  console.log('Registro exitoso');
-  return json;
-}
-*/
-
 export const createUser = async (name, email, password) =>{
     let response = await fetch(`${api}/api/users`, {
       method: 'POST',
@@ -45,7 +13,8 @@ export const createUser = async (name, email, password) =>{
         password: password
       })
     });
-    return response.json();
+    let json = await response.json(); 
+    return json;
   }
 
 export const loginUser = async (email, password) =>{
@@ -60,13 +29,10 @@ export const loginUser = async (email, password) =>{
       })
     });
     let json = await response.json();
-    console.log(json);
-    console.log('Inicio seccion exitoso');
     return json;
   }
 
   export const getUsers = async (access) =>{
-    console.log(access);
     let response = await fetch(`${api}/api/users`, {
       headers: {
         'Content-Type': 'application/json',
@@ -74,8 +40,35 @@ export const loginUser = async (email, password) =>{
       }
     });
     let json = await response.json();
-    console.log(json);
     return json;
+  }
+
+  export const upDateUser = async(email ,name, password, id, access) =>{
+    let response = await fetch(`${api}/api/users/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${access}`
+      },
+      body: JSON.stringify({
+        email: email,
+        name: name,
+        password: password
+      })
+    });
+
+    let json = await response.json();
+    return json
+  }
+
+  export const deleteUser = async( id, access) =>{
+    await fetch(`${api}/api/users/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${access}`
+      }
+    });
+
   }
 
  
