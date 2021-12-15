74a7e297749fcc96c4651c335643db473610b1eb
Majc23 <76822349+Majc23@users.noreply.github.com>
2021-12-15 16:48:02 -0600

    Update users.js

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,14 +1,13 @@
-import { getUsers } from "../API";
+import { getUsers, upDateUser, deleteUser } from "../API";
 import { types } from "../types/types";
+import { logout } from "./auth";
 
 export const startLoadingUsers = ( accessToken ) => {
     return async( dispatch ) => {
         
         const users = await getUsers( accessToken );
         let array1 = users.users;
-        console.log(array1);
         dispatch( setUsers( array1 ) );
-        console.log(array1);
 
     }
 }
@@ -26,21 +25,41 @@ export const activeUser = ( id, user ) => ({
     }
 });
 
-export const activeNote = ( id, user ) => ({
-    type: types.usersActive,
+
+
+export const startSaveUser = ( user ) => {
+    return async( dispatch ) => {
+
+        const { email, name, password, id } = user;
+        let access = JSON.parse(localStorage.getItem("acessToken"));
+       let newInf = await upDateUser(email,name, password, id, access.accessToken); 
+        console.log(newInf)
+
+        dispatch( refreshInf(id, newInf ) );
+    }
+}
+
+export const refreshInf = ( id, user ) => ({
+    type: types.usersUpdated,
     payload: {
         id,
-        ...user
+        user: {
+            id,
+            ...user
+        }
     }
 });
-/*
-export const startDeleting = ( id ) => {
-    return async( dispatch, getState ) => {
-         
-        const uid = getState().auth.uid;
-        await db.doc(`${ uid }/bookshelf/notes/${ id }`).delete();
 
+
+export const startDeleting = ( id ) => {
+    return async( dispatch ) => {
+         
+        let access = JSON.parse(localStorage.getItem("acessToken"));
+        await deleteUser(id, access.accessToken);
+
+        
         dispatch( deleteNote(id) );
+        dispatch( logout() );
 
     }
 }
@@ -48,4 +67,4 @@ export const startDeleting = ( id ) => {
 export const deleteNote = (id) => ({
     type: types.usersDelete,
     payload: id
-});*/
+});
